2b8154b8b13e6bf0760614c2529e439a9e00326b
Maximus Chen <Mr.MaximusChen@gmail.com>
2023-03-29 16:45:23 -0700

    passed all test in prototypal and pseudoclassical for stack.js and queue.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.keyNum = 0;
+  instance.storage = {};
+  return instance;
 };
 
 var stackMethods = {};
+stackMethods.push = function(value) {
+  var keyToPush = this.keyNum + 1;
+  this.storage[keyToPush] = value;
+  this.keyNum = keyToPush;
+};
 
+stackMethods.pop = function() {
+  var result = this.storage[this.keyNum];
+  delete this.storage[this.keyNum];
+  this.keyNum = this.keyNum - 1;
+  return result;
+};
+
+stackMethods.size = function() {
+  var totalKeys = 0;
+  for (var keys in this.storage) {
+    totalKeys++;
+  }
+  return totalKeys;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.keyNum = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  var keyToPush = this.keyNum + 1;
+  this.storage[keyToPush] = value;
+  this.keyNum = keyToPush;
+};
 
+Stack.prototype.pop = function() {
+  var result = this.storage[this.keyNum];
+  delete this.storage[this.keyNum];
+  this.keyNum = this.keyNum - 1;
+  return result;
+};
+
+Stack.prototype.size = function() {
+  var totalKeys = 0;
+  for (var keys in this.storage) {
+    totalKeys++;
+  }
+  return totalKeys;
+};
+
+var newStack = new Stack;
