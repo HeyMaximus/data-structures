5a74c20bef0a0da2bb839ee657fa9e27d654b702
Maximus Chen <Mr.MaximusChen@gmail.com>
2023-03-29 12:50:31 -0700

    passed all test for functional stack and queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = {
+    currentHead: 1,
+    lastKey: 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +10,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var enqueueKey = someInstance.lastKey + 1;
+    storage[enqueueKey] = value;
+    someInstance.lastKey = enqueueKey;
   };
 
   someInstance.dequeue = function() {
+    var result = storage[someInstance.currentHead];
+    delete storage[someInstance.currentHead];
+    someInstance.currentHead = someInstance.currentHead + 1;
+    return result;
   };
 
   someInstance.size = function() {
+    var keyTotal = 0;
+    for (var key in storage) {
+      keyTotal++;
+    }
+    return keyTotal;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,35 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+    numOfKeys: 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var nextPushKeyNum = someInstance.numOfKeys + 1;
+    storage[nextPushKeyNum] = value;
+    someInstance.numOfKeys = nextPushKeyNum;
   };
 
   someInstance.pop = function() {
+    var nextPopKeyNum = someInstance.numOfKeys;
+    var result = storage[nextPopKeyNum];
+    delete storage[nextPopKeyNum];
+    someInstance.numOfKeys = nextPopKeyNum - 1;
+    if (someInstance.numOfKeys < 0) {
+      someInstance.numOfKeys = 0;
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    var keyTotal = 0;
+    for (var key in storage) {
+      keyTotal++;
+    }
+    return keyTotal;
   };
 
   return someInstance;
