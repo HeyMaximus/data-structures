a99b03b4143964f6119b8c1d8bffa74d0493f186
Maximus Chen <Mr.MaximusChen@gmail.com>
2023-03-31 17:31:35 -0700

    passed all BMR with Xintong Mi.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,55 @@
 var BinarySearchTree = function(value) {
+  var _storage = Object.create(BinarySearchTree.methodes);
+  _storage.left = null;
+  _storage.right = null;
+  _storage.value = value;
+  return _storage;
+};
+
+BinarySearchTree.methodes = {};
+
+BinarySearchTree.methodes.insert = function(value) {
+  var rootNode = this;
+  var newNode = BinarySearchTree(value);
+  while (rootNode) {
+    if (value < rootNode.value) {
+      if (!rootNode.left) {
+        rootNode.left = newNode;
+        return;
+      }
+      rootNode = rootNode.left;
+    } else if (value > rootNode.value) {
+      if (!rootNode.right) {
+        rootNode.right = newNode;
+        return;
+      }
+      rootNode = rootNode.right;
+    }
+  }
+};
+
+BinarySearchTree.methodes.contains = function(value) {
+  var rootNode = this;
+  while (rootNode) {
+    if (value < rootNode.value) {
+      rootNode = rootNode.left;
+    } else if (value > rootNode.value) {
+      rootNode = rootNode.right;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
+
+BinarySearchTree.methodes.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,22 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage.get(index)) {
+    var obj = {};
+    this._storage.set(index, obj);
+  }
+  this._storage.get(index)[k] = v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage.get(index)[k];
+
 };
 
 
